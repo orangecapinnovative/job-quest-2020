a8f2e21a8c51c93aad89ebda02ba82d6d05277cb
lagmanzaza <lagmanmc@gmail.com>
2018-06-25 22:05:19 +0700

    [Route , Mongoose] route that provide deleting a joke

--- backend/src/routes/jokes.js
+++ backend/src/routes/jokes.js
@@ -7,15 +7,51 @@ export default (fastify, opt, next) => {
     })
     fastify.get('/:id', async (req, rest) => {
         const data = await model.findById(req.params.id)
-        
+
         res.status(200).send(data)
     })
-    fastify.post('/', async(req , res)=>{
+    fastify.post('/', async (req, res) => {
         const data = await model.insertMany({
             userId: req.body.userId,
             joke: req.body.joke
         })
         res.status(200).send(data)
+    })
+    fastify.delete('/:id', async (req, rest) => {
+        try {
+            await model.findByIdAndRemove(req.params.id)
+            res.status(200).send('removed!');
+        } catch (error) {
+            throw Error('joke not found')
+        }
+    })
+
+    fastify.post('/:id/like', async (req, res) => {
+        try {
+            const data = await model.findOne({
+                _id: req.params.id,
+                listUser: req.body.userId
+
+            })
+            if (data == null) {
+                await model.updateOne({
+                    _id: req.params.id
+                }, {
+                    $inc: {
+                        like: 1
+                    },
+                    $push: {
+                        listUser: req.body.userId
+                    }
+                })
+                res.send('liked!')
+            } else if (data != null) {
+                res.send('protecting spamming')
+            }
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
     })
     next()
 }
